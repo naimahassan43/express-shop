09d506bbaf57876094796f55bbc6aeea534c6952
Naima Hassan <naima.hassan434@gmail.com>
2022-02-23 00:37:01 +0000

    Server setup with declaring all routes with Express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const bicycles = require("./data/data.json");
+
+//Routes
+app.get("/", (req, res) => {
+  return res.send(bicycles);
+});
+app.get("/bicycle", (req, res) => {
+  console.log(req.query.id);
+  const bicycle = bicycles.find((b) => b.id === req.query.id);
+  return res.send(bicycle);
+});
+
+app.listen(3000, () => console.log("Server running"));
